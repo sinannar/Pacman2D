27a7d18d48dac9953981ea2fc36fcc1ad9989738
sinannar <sinan.nar@gmail.com>
2015-08-23 16:54:07 +1200

    Initial Commit

---
+++ Pacman2D/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Pacman2D
+//
+//  Created by Sinan on 23/08/15.
+//  Copyright (c) 2015 Sinan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
